e101d2952a99ba8ac0968a19e03acf3fedb72bd6
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.com>
2021-03-24 00:26:10 -0400

    segundo commit

---
+++ programa1.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+int main ()
+{
+        //Declaracion de variables locales:
+        int a;
+        int b;
+        int c;
+
+        //iniciar valriables:
+        a = 10;
+        b = 20;
+        c = a + b;
+
+        //Imprime por pantalla  el valor de  la variable c
+        cout << c; 
+	cout << "/n";  //imprime salto de linea
+
+        return 0;
+}
