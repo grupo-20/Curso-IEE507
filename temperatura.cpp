1566fb085deae091a440967ad9f9e07e69961951
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.com>
2021-04-01 01:56:58 -0400

    tarea2

---
+++ temperatura.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+using namespace std; 
+int S;
+
+main(){
+	
+	cout <<"Ingrese N° de sonidos emitidos"<<endl;
+	cin >>S;
+
+	cout <<"La temperatura en grados farenheit es:"<<end1;	
+cout<<(S/4)+402.1<<endl;
+	
+	return 0;
+}
